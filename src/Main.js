7c35913298c47c2378d8005092863d32dc885145
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-11-16 21:07:20 +0100

    Add images

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ArticlesList from './articles/ArticlesList'
+import SingleArticle from './articles/SingleArticle'
 export default class Main extends React.Component {
   render () {
     return (
-      <ArticlesList />
+      <SingleArticle />
     )
   }
 }
--- src/articles/ArticleListItem.js
+++ src/articles/ArticleListItem.js
@@ -10,14 +10,11 @@ export default class ArticleListItem extends React.Component {
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
           <p className='meta'>
+            <img src='/static/images/kgar.jpg' alt='Khaled Garbaya' className='user-icon user-1' />
             <a href='/author/khaled_garbaya/'>Khaled Garbaya</a>
-             <i className='link-spacer'></i>
+            <i className='link-spacer'></i>
             <time className='post-date' dateTime='2015-10-04'>Nov 2015</time>
           </p>
-        </div>
-
-        <div className='col-xs-2  no-gutter'>
-          <img src='/static/images/kgar.jpg' alt='Khaled Garbaya' className='user-icon user-1' />
         </div>
       </article>
     )
