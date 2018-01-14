5f155629008574d5a884444fb679ee29df55b4b8
Khaled Garbaya <khaled@contentful.com>
2018-01-14 12:47:06 +0100

    Add styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/Header'
-import './index.css'
+
+
+import "../styles/main.scss"
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Khaled Garbaya - doing Javascript and stuff!"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Khaled Garbaya is a software developer and active open sorcerer. He speaks multiple languages and is often overheard saying "Bonjour" in HTML' },
+        { name: 'keywords', content: 'Khaled Garbaya, javascript' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
       {children()}
-    </div>
   </div>
 )
 
