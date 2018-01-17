ebc7171dc581831e61ff44cd7bb49c5cacddcd34
Khaled Garbaya <khaled@contentful.com>
2018-01-17 23:04:32 +0100

    Change footer border

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,12 @@ const Footer = () => (
     </small>
       <br />
       <br />
-<a href='https://www.contentful.com/' rel='nofollow' target='_blank'><img src='https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg' style={{maxWidth:'100px',width:'100%'}} alt="Powered by Contentful" /></a>
+      <a href='https://www.contentful.com/' rel='nofollow' target='_blank'>
+        <img 
+          src='https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg'
+          style={{maxWidth:'100px',width:'100%'}} 
+          alt="Powered by Contentful" />
+      </a>
     </footer>
 )
 
