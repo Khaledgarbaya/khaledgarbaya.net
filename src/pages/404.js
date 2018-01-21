5e8095dc3e731189ad04eaadb06f27df99398697
Khaled Garbaya <khaled@contentful.com>
2018-01-21 13:38:13 +0100

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div className='main-content'>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className='main-content notFound'>
+  <div className='notFound__text'>
+    Oh welcome to an endless supply of my rotating head,you can stay here watching this or go back to the <Link to='/'>home</Link> page
+  </div>
+  <div className='notFound__animation'>
+    <span className='notFound__four-left'>4</span>
+    <img className='notFound__img' width='300' height='300' src='logo.svg' />
+    <span className='notFound__four-right'>4</span>
+  </div>
   </div>
 )
 
