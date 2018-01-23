96a144e1000835b8f23dd15e087a25152f27b275
Khaled Garbaya <khaled@contentful.com>
2018-01-23 23:17:57 +0100

    Add about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import * as PropTypes from 'prop-types';
 
-const AboutPage = () => (
+const AboutPage = ({data}) => (
   <div className="about main-content">
-    <h1>About</h1>
+    <img
+      className="about__image"
+      src={
+        data.allContentfulLandingPage.edges[0].node.image.responsiveResolution.src
+      }
+      alt=""
+    />
+    <div
+      className="about__content"
+      dangerouslySetInnerHTML={{
+        __html:
+          data.allContentfulLandingPage.edges[0].node.content
+            .childMarkdownRemark.html,
+      }}
+    />
   </div>
 )
 
+AboutPage.propTypes = {
+  data: PropTypes.object.isRequired
+}
+
 export default AboutPage
+
+export const pageQuery = graphql`
+  query AboutPageQuery {
+    allContentfulLandingPage(filter: {slug: {eq: "about"}}) {
+      edges {
+        node {
+          content {
+            childMarkdownRemark {
+                html
+              }
+            }
+            image {
+              responsiveResolution (width:400) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+`
