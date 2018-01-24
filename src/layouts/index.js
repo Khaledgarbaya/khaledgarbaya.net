a9604c1423a92cddad3c41d8d4b45926283a883d
Khaled Garbaya <khaled@contentful.com>
2018-01-24 14:39:01 +0100

    fix favicons link

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,19 +15,19 @@ const TemplateWrapper = ({ children }) => (
         {
           rel: 'apple-touch-icon',
           type: 'image/png',
-          href: './favicons/apple-touch-icon.png'
+          href: '/favicons/apple-touch-icon.png'
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '32x32',
-          href: './favicons/favicon-32x32.png'
+          href: '/favicons/favicon-32x32.png'
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '16x16',
-          href: './favicons/favicon-16x16.png'
+          href: '/favicons/favicon-16x16.png'
         }
       ]}
       meta={[
