797d0c641f1c62a09ec2e2296e97d5f78c65b907
Khaled Garbaya <khaledgarbaya@gmail.com>
2018-06-08 10:05:23 +0200

    Add form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const ContactPage = () => (
-  <div className="contact main-content">
-    <h2>Wanna say hi?</h2>
-    <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a></p>
+  <div className='contact main-content'>
+    <h1>Contact</h1>
+    <p>Wanna say hi? or have a question</p>
+    <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a> or send me a message through the form bellow.</p>
+    <form name='contact' className='contact__form' method='post' data-netlify='true'>
+      <label for='name'>
+        Name
+        <input id='name' name='name' type='text' />
+      </label>
+      <label for='email'>
+        Email
+        <input id='email' name='email' type='email' />
+      </label>
+      <label for='name'>
+        Message
+        <textarea id='name' name='message'/>
+      </label>
+      <div data-netlify-recaptcha='true'></div>
+      <button>Send</button>
+    </form>
   </div>
 )
 
