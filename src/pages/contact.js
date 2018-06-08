9f56be0b7c51b0f06e2409bf089f5a8d5fedcb21
Khaled Garbaya <khaledgarbaya@gmail.com>
2018-06-08 10:10:50 +0200

    Fix form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => (
     <h1>Contact</h1>
     <p>Wanna say hi? or have a question</p>
     <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a> or send me a message through the form bellow.</p>
-    <form name='contact' className='contact__form' method='post' data-netlify='true'>
+    <form name='contact' className='contact__form' method='post' netlify-honeypot="bot-field" netlify data-netlify='true'>
       <label for='name'>
         Name
         <input id='name' name='name' type='text' />
@@ -18,7 +18,6 @@ const ContactPage = () => (
         Message
         <textarea id='name' name='message'/>
       </label>
-      <div data-netlify-recaptcha='true'></div>
       <button>Send</button>
     </form>
   </div>
