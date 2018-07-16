36b8387b7c07ab2b69e866caf06963f00b7208a4
Khaled Garbaya <khaled@contentful.com>
2018-07-16 15:15:57 +0200

    Add reCAPTCHA2

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 const ContactPage = () => (
   <div className='contact main-content'>
+    <Helmet>
+      <script src="https://www.google.com/recaptcha/api.js"></script>
+    </Helmet>
     <h1>Contact</h1>
     <p>Wanna say hi? or have a question</p>
     <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a> or send me a message through the form bellow.</p>
-    <form name='contact' className='contact__form' method='post' data-netlify-honeypot={'bot-field'} data-netlify='true'>
+    <form name='contact' className='contact__form' method='post' data-netlify-recaptcha data-netlify='true'>
 
       <input type="hidden" name="form-name" value="contact" />
       <label aria-hidden >Don’t fill this out if you're human: <input name="bot-field" /></label>
@@ -21,6 +25,7 @@ const ContactPage = () => (
         Message
         <textarea id='name' name='message'/>
       </label>
+      <div className="g-recaptcha" data-sitekey="6LcQc2QUAAAAALAD81iQy5Gy8pu5xSwYzhQPsBmI"></div>
       <button>Send</button>
     </form>
   </div>
