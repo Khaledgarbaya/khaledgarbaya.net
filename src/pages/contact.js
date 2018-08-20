89e6cc98a29f93aac85ff4e53c110af6c1553a77
Khaled Garbaya <khaled@contentful.com>
2018-08-20 12:20:38 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,15 +3,12 @@ import Helmet from 'react-helmet'
 
 const ContactPage = () => (
   <div className='contact main-content'>
-    <Helmet>
-      <script src="https://www.google.com/recaptcha/api.js"></script>
-    </Helmet>
     <h1>Contact</h1>
     <p>Wanna say hi? or have a question</p>
     <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a> or send me a message through the form bellow.</p>
-    <form name='contact' className='contact__form' method='post' data-netlify-recaptcha data-netlify='true'>
+    <form name='contact' className='contact__form'  method='post' data-netlify/>
 
-      <input type="hidden" name="form-name" value="contact" />
+      <input type="hidden" name="form-name" value="contact">
       <label aria-hidden >Don’t fill this out if you're human: <input name="bot-field" /></label>
       <label for='name'>
         Name
@@ -25,7 +22,6 @@ const ContactPage = () => (
         Message
         <textarea id='name' name='message'/>
       </label>
-      <div className="g-recaptcha" data-sitekey="6LcQc2QUAAAAALAD81iQy5Gy8pu5xSwYzhQPsBmI"></div>
       <button>Send</button>
     </form>
   </div>
