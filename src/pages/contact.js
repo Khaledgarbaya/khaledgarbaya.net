2bee86649bd245831e8f44359b40022c27194dfd
Khaled Garbaya <khaled@contentful.com>
2018-08-20 12:23:36 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,19 +6,17 @@ const ContactPage = () => (
     <h1>Contact</h1>
     <p>Wanna say hi? or have a question</p>
     <p>Hit me up on twitter <a href='https://twitter.com/khaled_garbaya'>@khaled_garbaya</a> or send me a message through the form bellow.</p>
-    <form name='contact' className='contact__form'  method='post' data-netlify/>
-
-      <input type="hidden" name="form-name" value="contact">
-      <label aria-hidden >Don’t fill this out if you're human: <input name="bot-field" /></label>
-      <label for='name'>
-        Name
-        <input id='name' name='name' type='text' />
-      </label>
-      <label for='email'>
-        Email
-        <input id='email' name='email' type='email' />
-      </label>
-      <label for='name'>
+   <form name='contact' className='contact__form' method='post' data-netlify>
+          <input type="hidden" name="form-name" value="contact" />
+          <label for='name'>
+            Name
+            <input id='name' name='name' type='text' />
+          </label>
+          <label for='email'>
+          Email
+          <input id='email' name='email' type='email' />
+        </label>
+        <label for='name'>
         Message
         <textarea id='name' name='message'/>
       </label>
