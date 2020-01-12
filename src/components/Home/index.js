2047aebf60489d6e44b9c7661fd2b1f9695968a1
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-01-12 21:05:23 +0100

    add SEO to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import Layout from '../Layout'
+import SEO from '../../components/SEO'
 
 const Article = ({ data }) => {
   return (
